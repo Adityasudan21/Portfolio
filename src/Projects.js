42d54c6abc06c18f823b807eaf510713d4015b84
Aditya Sudan <adityasudan21@gmail.com>
2024-01-18 02:16:08 +0530

    Created Projects project prototype

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import "./Projects.css";
 import InfoIcon from '@mui/icons-material/Info';
 function Projects() {
+    const [showFirstImage, setShowFirstImage] = useState(true);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setShowFirstImage((prev) => !prev);
+        }, 3000); // Change the interval time as needed
+
+        return () => clearInterval(intervalId);
+    }, []);
     return (
         <div className='projects'>
             <div className="heading_projects">
@@ -9,7 +18,8 @@ function Projects() {
             </div>
             <div className="project1">
                 <div className="connect-turret-image">
-                    <img src="https://i.ibb.co/p37vTFC/Screenshot-2024-01-18-004739.png" alt="Front Image" />
+                    <img className={`connect-turret-image-transition ${showFirstImage ? 'first' : 'second'}`}
+                        src={showFirstImage ? "https://i.ibb.co/p37vTFC/Screenshot-2024-01-18-004739.png" : "https://i.ibb.co/6DHBMws/Screenshot-2024-01-18-004819.png"} alt="Front Image" />
 
                 </div>
                 <div className="connect-turret-description">
