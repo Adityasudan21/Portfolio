a1c7ce1b16be1cdeddb7695af0a79561cae4edad
Aditya Sudan <adityasudan21@gmail.com>
2024-01-17 23:52:51 +0530

    Simple TechStack menu Made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+import TechStack from './TechStack';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Main />
+      <TechStack />
     </div>
   );
 }
